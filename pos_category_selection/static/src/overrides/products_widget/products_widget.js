107a7478206cf8985ba16f3a94e8c88bb35cab11
Amrithesh K <amritheshvenugopalan@gmail.com>
2025-01-28 17:30:47 +0530

    [ADD] add sub category functionality in pos [UPDT] update total price functionality

--- pos_category_selection/static/src/overrides/products_widget/products_widget.js
+++ pos_category_selection/static/src/overrides/products_widget/products_widget.js
@@ -5,25 +5,67 @@ import { patch } from "@web/core/utils/patch";
 import { SelectCategory } from "../../components/select_category/select_category"
 const { useState } = owl
 
-ProductsWidget.components = { ...ProductsWidget.components,  SelectCategory};
+ProductsWidget.components = { ...ProductsWidget.components, SelectCategory };
 patch(ProductsWidget.prototype, {
     setup() {
         super.setup();
-        this.state = useState({...this.state, categorySelected: false});
+        this.state = useState({
+            ...this.state,
+            categorySelected: false,
+            subcategorySelected: false,
+            currentCategoryId: 0,
+        });
     },
+
     onSelectCategory(selectedCategory) {
-        this.pos.setSelectedCategoryId(selectedCategory)
-        this.state.categorySelected = true;
+        this.state.currentCategoryId = selectedCategory;
+        const hasSubcategories = this.pos.db.get_category_childs_ids(selectedCategory).length > 0;
+
+        if (hasSubcategories) {
+            this.state.categorySelected = true;
+            this.state.subcategorySelected = false;
+        } else {
+            this.state.categorySelected = true;
+            this.state.subcategorySelected = true;
+            this.pos.setSelectedCategoryId(selectedCategory);
+        }
     },
-    onClickBack () {
-        this.state.categorySelected = false;
+
+
+    onSelectSubcategory(selectedCategory) {
+        this.state.subcategorySelected = true;
+        this.pos.setSelectedCategoryId(selectedCategory);
     },
-    getShowCategoryImages () {
-        return false
+
+    onClickBack() {
+        const hasSubcategories = this.pos.db.get_category_childs_ids(this.state.currentCategoryId).length > 0;
+
+        if (!hasSubcategories) {
+            this.state.categorySelected = false;
+            this.state.subcategorySelected = false;
+            this.state.currentCategoryId = 0;
+            this.pos.setSelectedCategoryId(0);
+        } else if (this.state.subcategorySelected) {
+            this.state.subcategorySelected = false;
+        } else {
+            this.state.categorySelected = false;
+            this.state.currentCategoryId = 0;
+            this.pos.setSelectedCategoryId(0);
+        }
     },
+
+    getShowCategoryImages() {
+        return false;
+    },
+
     get categorySelected() {
         return this.state.categorySelected;
     },
+
+    get subcategorySelected() {
+        return this.state.subcategorySelected;
+    },
+
     get categoriesForSelector() {
         return [
             ...this.pos.db.get_category_ancestors_ids(0),
@@ -33,19 +75,27 @@ patch(ProductsWidget.prototype, {
             .map((id) => this.pos.db.category_by_id[id])
             .map((category) => {
                 const isRootCategory = category.id === this.pos.db.root_category_id;
-                const showSeparator =
-                    !isRootCategory &&
-                    [
-                        ...this.pos.db.get_category_ancestors_ids(this.pos.selectedCategoryId),
-                        this.pos.selectedCategoryId,
-                    ].includes(category.id);
                 return {
                     id: category.id,
                     name: !isRootCategory ? category.name : "",
                     imageUrl:
                         category?.has_image &&
                         `/web/image?model=pos.category&field=image_128&id=${category.id}&unique=${category.write_date}`,
+                    hasChildren: this.pos.db.get_category_childs_ids(category.id).length > 0,
                 };
             });
-    }
-})
+    },
+
+    get subcategoriesForSelector() {
+        return this.pos.db.get_category_childs_ids(this.state.currentCategoryId)
+            .map((id) => this.pos.db.category_by_id[id])
+            .map((category) => ({
+                id: category.id,
+                name: category.name,
+                imageUrl:
+                    category?.has_image &&
+                    `/web/image?model=pos.category&field=image_128&id=${category.id}&unique=${category.write_date}`,
+                hasChildren: this.pos.db.get_category_childs_ids(category.id).length > 0,
+            }));
+    },
+});
--- pos_product_total_price/models/product_template.py
+++ pos_product_total_price/models/product_template.py
@@ -10,16 +10,18 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     total_price = fields.Float(
-        string="Total Price",
+        string="Total Price(Including VAT)",
+        compute='_compute_total_price',
+        inverse='_inverse_total_price',
+        store=True,
         digits='Product Price',
-        help="Total price of the product including applicable taxes."
+        help="Total price including taxes. This price will be used in POS."
     )
 
-    @api.onchange('list_price', 'taxes_id')
-    def _onchange_list_price(self):
+    @api.depends('list_price', 'taxes_id')
+    def _compute_total_price(self):
         """
-            Compute and update the total price of the product when either the
-            list price or associated taxes are modified.
+        When list_price changes, compute the total price with taxes.
         """
         for product in self:
             if not product.taxes_id:
@@ -33,3 +35,31 @@ class ProductTemplate(models.Model):
                     partner=self.env['res.partner']
                 )
                 product.total_price = taxes['total_included']
+
+    def _inverse_total_price(self):
+        """
+        When total_price is modified, compute and update list_price by removing taxes.
+        """
+        for product in self:
+            if not product.taxes_id:
+                product.list_price = product.total_price
+            else:
+                tax_factor = 1.0
+                for tax in product.taxes_id:
+                    if tax.amount_type == 'percent':
+                        tax_factor += tax.amount / 100.0
+                product.list_price = product.total_price / tax_factor
+
+    @api.onchange('total_price')
+    def _onchange_total_price(self):
+        """
+        Instantly update list_price when total_price changes in the UI.
+        """
+        if not self.taxes_id:
+            self.list_price = self.total_price
+        else:
+            tax_factor = 1.0
+            for tax in self.taxes_id:
+                if tax.amount_type == 'percent':
+                    tax_factor += tax.amount / 100.0
+            self.list_price = self.total_price / tax_factor
