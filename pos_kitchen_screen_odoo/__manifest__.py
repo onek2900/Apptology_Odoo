0af218a6faf0516032288ca64515664f0eff3f89
onek2900 <98646377+onek2900@users.noreply.github.com>
2025-09-27 08:31:51 -0400

    updatekitchen

--- pos_kitchen_screen_odoo/__manifest__.py
+++ pos_kitchen_screen_odoo/__manifest__.py
@@ -23,60 +23,20 @@
             'pos_kitchen_screen_odoo/static/src/js/navbar.js',
             'pos_kitchen_screen_odoo/static/src/xml/navbar.xml'
         ],
-        'web.assets_backend': [
-            'pos_kitchen_screen_odoo/static/src/css/kitchen.scss',
-            'pos_kitchen_screen_odoo/static/src/js/kitchen_screen.js',
-            'pos_kitchen_screen_odoo/static/src/xml/kitchen_screen_templates.xml',
-        ],
-        'apptology_order_tracking.assets': [
-            # Web helpers and core assets
-            ("include", "web._assets_helpers"),
-            ("include", "web._assets_backend_helpers"),
-            ("include", "web._assets_primary_variables"),
-
-            # Bootstrap core
-            "web/static/src/scss/pre_variables.scss",
-            "web/static/lib/bootstrap/scss/_functions.scss",
-            "web/static/lib/bootstrap/scss/_variables.scss",
-            ("include", "web._assets_bootstrap"),
-
-            'web/static/src/libs/fontawesome/css/font-awesome.css',
-            'web/static/lib/odoo_ui_icons/*',
-            'web/static/src/webclient/navbar/navbar.scss',
-            'web/static/src/scss/animation.scss',
-            'web/static/src/scss/fontawesome_overridden.scss',
-            'web/static/src/scss/mimetypes.scss',
-            'web/static/src/scss/ui.scss',
-            'web/static/src/views/fields/translation_dialog.scss',
-            'web/static/src/legacy/scss/ui.scss',
-
-            # Core web assets
-            ('include', 'web._assets_core'),
-
-            # Remove unnecessary components
-            ("remove", "web/static/src/core/debug/**/*"),
-
-            # UI components
-            "web/static/lib/odoo_ui_icons/*",
-            'web/static/src/legacy/scss/ui.scss',
-
+        # Load the standalone kitchen page via frontend assets instead of a custom bundle
+        'web.assets_frontend': [
+            # Ensure bus service is available on the standalone page
             'bus/static/src/*.js',
             'bus/static/src/services/**/*.js',
             'bus/static/src/workers/websocket_worker.js',
             'bus/static/src/workers/websocket_worker_utils.js',
-            "point_of_sale/static/src/utils.js",
 
-            # Module specific assets
-            'pos_kitchen_screen_odoo/static/src/css/order_screen.scss',
-            'pos_kitchen_screen_odoo/static/src/xml/order_screen.xml',
-            'pos_kitchen_screen_odoo/static/src/js/order_screen.js',
-
+            # Kitchen app assets
             'pos_kitchen_screen_odoo/static/src/css/kitchen.scss',
             'pos_kitchen_screen_odoo/static/src/js/kitchen_screen.js',
             'pos_kitchen_screen_odoo/static/src/xml/kitchen_screen_templates.xml',
-
             'pos_kitchen_screen_odoo/static/src/js/mountApps.js',
-        ]
+        ],
     },
     'license': 'LGPL-3',
     'installable': True,
